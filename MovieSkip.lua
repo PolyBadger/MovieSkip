50b4a6c06f636957f9d50e9bf3e976567b3e9065
PolyBadger <polybadger1337@gmail.com>
2025-07-30 13:01:10 -0600

    Update Version

--- MovieSkip.lua
+++ MovieSkip.lua
@@ -1,23 +1,42 @@
 
 local color = "|c006699ff"
+local message = {
+	skip = "Cinematic skipped, play with /MovieSkip no",
+	play = "Cinematic played, skip with /MovieSkip yes",
+}
+
+StaticPopupDialogs["MovieSkipDialog"] = {
+	text = "\nMovieSkip\n\n%s, do you want to play or skip cinematics?\n\n",
+	button1 = "Play",
+	button2 = "Skip",
+	OnAccept = function()
+		MovieSkipWTF.skip = false
+	end,
+	OnCancel = function()
+		MovieSkipWTF.skip = true
+	end,
+}
 
 local PlayMovie = MovieFrame_PlayMovie
 MovieFrame_PlayMovie = function(self, movieID)
 	MovieSkipWTF.replay = movieID
-	if(MovieSkipWTF.skip) then
+	if MovieSkipWTF.skip then
 		MovieFrame_StopMovie(self)
-		print(color.."MovieSkip: cinematic skipped, play with /MovieSkip replay")
+		print(color..message.skip)
 	else
 		PlayMovie(self, movieID)
+		print(color..message.play)
 	end
 end
 
 CinematicFrame:HookScript("OnShow", function(self, ...)
 	-- No movie ID provided for Cinematics =(
 	MovieSkipWTF.replay = 0	
-	if(MovieSkipWTF.skip) then
+	if MovieSkipWTF.skip then
 		CinematicFrame_CancelCinematic()
-		print(color.."MovieSkip: cinematic skipped")
+		print(color..message.skip)
+	else
+		print(color..message.play)	
 	end
 end)
 
@@ -30,17 +49,16 @@ local DefaultWTF = {
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(self, event, ...)
-	if(event == "ADDON_LOADED" and ... == "MovieSkip") then
-		if(MovieSkipWTF == nil or type(MovieSkipWTF) == "boolean") then
+	if event == "ADDON_LOADED" and ... == "MovieSkip" then
+		if MovieSkipWTF == nil or type(MovieSkipWTF) == "boolean" then
 			-- Initialize WTF on first time install
 			MovieSkipWTF = DefaultWTF
 		end
 		-- Upgrade WTF on new build
 		local v, b, d, tocv = GetBuildInfo()
-		if(MovieSkipWTF.tocv < tocv) then
+		if MovieSkipWTF.tocv < tocv then
 			MovieSkipWTF.tocv = tocv				
-			MovieSkipWTF.skip = false
-			print(color.."Cinematics will play for new build, skip with /MovieSkip yes")					
+			StaticPopup_Show("MovieSkipDialog", "A new build has been installed")
 		end
 		MovieSkipWTF.replay = 0
 	end
@@ -48,23 +66,21 @@ end)
 
 SLASH_MOVIESKIP1 = "/movieskip"
 SlashCmdList["MOVIESKIP"] = function(msg)
-	if(msg == "replay") then
-		if(MovieSkipWTF.replay > 0) then
+	if msg == "replay" then
+		if MovieSkipWTF.replay > 0 then
 			PlayMovie(MovieFrame, MovieSkipWTF.replay)
 		else
-			print(color.."MovieSkip: unable to replay cinematic")
+			print(color.."MovieSkip is unable to replay cinematic")
 		end
 		return
-	elseif (msg == "yes") then
+	elseif msg == "yes" or msg == "skip" then
 		MovieSkipWTF.skip = true
-	elseif (msg == "no") then
+	elseif msg == "no" or msg == "play" then
 		MovieSkipWTF.skip = false
-	end
-	
-	if(MovieSkipWTF.skip) then
-		print(color.."Cinematics are skipped, play with /MovieSkip no")
+	elseif MovieSkipWTF.skip then
+		StaticPopup_Show("MovieSkipDialog", "Cinematics are currently skipped")
 	else
-		print(color.."Cinematics will play, skip with /MovieSkip yes")
+		StaticPopup_Show("MovieSkipDialog", "Cinematics are currently playing")		
 	end
 end
 
